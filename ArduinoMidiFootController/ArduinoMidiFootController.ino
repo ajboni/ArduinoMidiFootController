765397f1155d0c566b99e2283469d5eebdaae389
rodobodolfo <peladovii@hotmail.com>
2018-05-05 15:14:25 -0300

    Add project files.

---
+++ ArduinoMidiFootController/ArduinoMidiFootController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoMidiFootController.ino
+ Created:	5/5/2018 3:14:17 PM
+ Author:	baj
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
